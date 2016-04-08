05219b8f185dd8ce32a8650f978b4cae91b78b25
serious-scribbler <contact@pniehus.de>
2016-04-08 19:42:54 +0200

    Added native exceptions
    The native code can now send Exceptions to java

--- src/include/AlienFXController.c
+++ src/include/AlienFXController.c
@@ -3,8 +3,9 @@
 #include <conio.h>
 #include "de_pniehus_jalienfx_AlienFXController.h"
 
+#define NULL_POINTER_ERROR				7 //NULL POINTER
 //Returns the number of AlienFX compatible devices connected to the system
-JNIEXPORT jint JNICALL Java_de_pniehus_jalienfx_AlienFXController_getDeviceCount(JNIEnv *, jobject){
+JNIEXPORT jint JNICALL Java_de_pniehus_jalienfx_AlienFXController_getDeviceCount(JNIEnv *env, jobject obj){
   jint result;
   unsigned int lfxDeviceNumber = 0;
 	LFX_GetNumDevices(&lfxDeviceNumber);
@@ -12,22 +13,51 @@ JNIEXPORT jint JNICALL Java_de_pniehus_jalienfx_AlienFXController_getDeviceCount
   return result;
 }
 
-JNIEXPORT jint JNICALL Java_de_pniehus_jalienfx_AlienFXController_getZCount(JNIEnv *, jobject, jint){
+//Returns the number of AlienFX zones for the selected device
+JNIEXPORT jint JNICALL Java_de_pniehus_jalienfx_AlienFXController_getZCount(JNIEnv *env, jobject obj, jint deviceID){
+  jint result;
+  if(deviceID == NULL) throwException(NULL_POINTER_ERROR, env);
+  unsigned int numberOfZones = 0;
+	int success = LFX_GetNumLights(deviceID, &numberOfZones);
+  if(success != LFX_SUCCESS) throwException(sucess, env);
+  result = (int) numberOfZones;
+  return result;
+}
+
+//Sends an Exception to java
+void throwException(int type, JNIEnv *env){
+  jclass Exception = env->FindClass("java/lang/Exception");
+  switch(type){
+    case NULL_POINTER_ERROR:
+      env->ThrowNew(Exception,"JAlienFX native Exception: NULL Pointer Exception!");
+      break;
+    case LFX_ERROR_NOINIT:
+      env->ThrowNew(Exception,"JAlienFX native Exception: AlienFX has not been initialized yet!");
+      break;
+    case LFX_ERROR_NODEVS:
+      env->ThrowNew(Exception,"JAlienFX native Exception: This device doesn't exist!");
+      break;
+    case LFX_ERROR_NOLIGHTS:
+      env->ThrowNew(Exception, "JAlienFX native Exception: This light doesn't exist!");
+      break;
+    default:
+      env->ThrowNew(Exception,"JAlienFX native Exception: Unspecified error!");
+      break;
+  }
+}
+
+JNIEXPORT jstring JNICALL Java_de_pniehus_jalienfx_AlienFXController_getDevDescription(JNIEnv *env, jobject obj, jint deviceID){
 
 }
 
-JNIEXPORT jstring JNICALL Java_de_pniehus_jalienfx_AlienFXController_getDevDescription(JNIEnv *, jobject, jint){
+JNIEXPORT jstring JNICALL Java_de_pniehus_jalienfx_AlienFXController_getZDescription(JNIEnv *env, jobject obj, jint deviceID, jint zone){
 
 }
 
-JNIEXPORT jstring JNICALL Java_de_pniehus_jalienfx_AlienFXController_getZDescription(JNIEnv *, jobject, jint, jint){
+JNIEXPORT jintArray JNICALL Java_de_pniehus_jalienfx_AlienFXController_getRGBA(JNIEnv *env, jobject obj, jint deviceID, jint zone){
 
 }
 
-JNIEXPORT jintArray JNICALL Java_de_pniehus_jalienfx_AlienFXController_getRGBA(JNIEnv *, jobject, jint, jint){
-
-}
-
-JNIEXPORT void JNICALL Java_de_pniehus_jalienfx_AlienFXController_setRGBA(JNIEnv *, jobject, jint, jint, jint, jint, jint, jint){
+JNIEXPORT void JNICALL Java_de_pniehus_jalienfx_AlienFXController_setRGBA(JNIEnv *env, jobject obj, jint deviceID, jint zone, jint red, jint green, jint blue, jint brightness){
 
 }
