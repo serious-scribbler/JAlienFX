395ab7c4829120ac09134f808b2d3188b93412f0
serious-scribbler <contact@pniehus.de>
2016-04-08 20:11:09 +0200

    Move throwException()-method fixed bugs

--- src/include/AlienFXController.c
+++ src/include/AlienFXController.c
@@ -4,12 +4,35 @@
 #include <conio.h>
 
 #define NULL_POINTER_ERROR				7 //NULL POINTER
+
+//Sends an Exception to java
+void throwException(int type, JNIEnv *env){
+  jclass Exception = (*env)->FindClass(env, "java/lang/Exception");
+  switch(type){
+    case NULL_POINTER_ERROR:
+      (*env)->ThrowNew(env, Exception,"JAlienFX native Exception: NULL Pointer Exception!");
+      break;
+    case LFX_ERROR_NOINIT:
+      (*env)->ThrowNew(env, Exception,"JAlienFX native Exception: AlienFX has not been initialized yet!");
+      break;
+    case LFX_ERROR_NODEVS:
+      (*env)->ThrowNew(env, Exception,"JAlienFX native Exception: This device doesn't exist!");
+      break;
+    case LFX_ERROR_NOLIGHTS:
+      (*env)->ThrowNew(env, Exception, "JAlienFX native Exception: This light doesn't exist!");
+      break;
+    default:
+      (*env)->ThrowNew(env, Exception,"JAlienFX native Exception: Unspecified error!");
+      break;
+  }
+}
+
 //Returns the number of AlienFX compatible devices connected to the system
 JNIEXPORT jint JNICALL Java_de_pniehus_jalienfx_AlienFXController_getDeviceCount(JNIEnv *env, jobject obj){
   jint result;
   unsigned int lfxDeviceNumber = 0;
 	int success = LFX_GetNumDevices(&lfxDeviceNumber);
-  throwException(success, env);
+  if(success != LFX_SUCCESS) throwException(success, env);
 	result = (int) lfxDeviceNumber;
   return result;
 }
@@ -20,32 +43,12 @@ JNIEXPORT jint JNICALL Java_de_pniehus_jalienfx_AlienFXController_getZCount(JNIE
   if(deviceID == NULL) throwException(NULL_POINTER_ERROR, env);
   unsigned int numberOfZones = 0;
 	int success = LFX_GetNumLights(deviceID, &numberOfZones);
-  if(success != LFX_SUCCESS) throwException(sucess, env);
+  if(success != LFX_SUCCESS) throwException(success, env);
   result = (int) numberOfZones;
   return result;
 }
 
-//Sends an Exception to java
-void throwException(int type, JNIEnv *env){
-  jclass Exception = env->FindClass("java/lang/Exception");
-  switch(type){
-    case NULL_POINTER_ERROR:
-      env->ThrowNew(Exception,"JAlienFX native Exception: NULL Pointer Exception!");
-      break;
-    case LFX_ERROR_NOINIT:
-      env->ThrowNew(Exception,"JAlienFX native Exception: AlienFX has not been initialized yet!");
-      break;
-    case LFX_ERROR_NODEVS:
-      env->ThrowNew(Exception,"JAlienFX native Exception: This device doesn't exist!");
-      break;
-    case LFX_ERROR_NOLIGHTS:
-      env->ThrowNew(Exception, "JAlienFX native Exception: This light doesn't exist!");
-      break;
-    default:
-      env->ThrowNew(Exception,"JAlienFX native Exception: Unspecified error!");
-      break;
-  }
-}
+
 
 JNIEXPORT jstring JNICALL Java_de_pniehus_jalienfx_AlienFXController_getDevDescription(JNIEnv *env, jobject obj, jint deviceID){
 
